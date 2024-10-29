091b0ee49bfe07b218ee473fae91caf2240ab76b
Debjit Mandal <115482933+opulentloop@users.noreply.github.com>
2024-10-29 20:33:47 +0530

    fix: Link Redirects fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const CARDS = [
   {
     name: "Website",
     icon: "ri-global-fill",
-    link: "https://opulentloop.is-a.dev",
+    link: "https://debjit.is-a.dev",
   },
   {
     name: "Gmail",
